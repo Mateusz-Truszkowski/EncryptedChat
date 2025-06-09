1d199e053a558e610d275d3beba657e024173a1c
Mateusz <mateusz.truszkowski002@gmail.com>
2025-06-09 22:29:51 +0200

    Add mappers

--- backend/src/main/java/com/chat/backend/mappers/impl/UserMapperImpl.java
+++ backend/src/main/java/com/chat/backend/mappers/impl/UserMapperImpl.java
@@ -1,4 +1,25 @@
 package com.chat.backend.mappers.impl;
 
-public class UserMapperImpl {
+import com.chat.backend.domain.dto.UserDto;
+import com.chat.backend.domain.entities.UserEntity;
+import com.chat.backend.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+
+public class UserMapperImpl implements Mapper<UserEntity, UserDto> {
+
+    private final ModelMapper modelMapper;
+
+    public UserMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public UserDto mapTo(UserEntity userEntity) {
+        return modelMapper.map(userEntity, UserDto.class);
+    }
+
+    @Override
+    public UserEntity mapFrom(UserDto userDto) {
+        return modelMapper.map(userDto, UserEntity.class);
+    }
 }
