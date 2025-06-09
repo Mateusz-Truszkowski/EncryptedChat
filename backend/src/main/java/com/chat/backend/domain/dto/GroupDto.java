2f7a29b837836cebfd7abfdc9910e30cb06b1932
Mateusz <mateusz.truszkowski002@gmail.com>
2025-06-09 22:13:51 +0200

    Add dtos

--- backend/src/main/java/com/chat/backend/domain/dto/GroupDto.java
+++ backend/src/main/java/com/chat/backend/domain/dto/GroupDto.java
@@ -1,4 +1,15 @@
 package com.chat.backend.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class GroupDto {
+    private Integer id;
+    private String name;
 }
--- backend/src/main/java/com/chat/backend/domain/dto/UserDto.java
+++ backend/src/main/java/com/chat/backend/domain/dto/UserDto.java
@@ -1,4 +1,19 @@
 package com.chat.backend.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class UserDto {
+    private Integer id;
+    private String username;
+    private String password;
+    private LocalDateTime last_activity;
 }
