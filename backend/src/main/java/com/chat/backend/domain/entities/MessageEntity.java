f2df5de393e9f5f266f022d2758df6a0dfc7ba98
Mateusz <mateusz.truszkowski002@gmail.com>
2025-06-09 22:01:27 +0200

    Add entities

--- backend/src/main/java/com/chat/backend/domain/entities/MessageEntity.java
+++ backend/src/main/java/com/chat/backend/domain/entities/MessageEntity.java
@@ -1,4 +1,32 @@
 package com.chat.backend.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name="messages")
 public class MessageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "message_id_seq")
+    private Long id;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "group_id")
+    private GroupEntity group;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "sender_id")
+    private UserEntity sender;
+    private String content;
+    private String encryption_key;
+    private LocalDateTime sent_at;
+    private String attachment;
+    private String status;
 }
--- backend/src/main/java/com/chat/backend/domain/entities/UserEntity.java
+++ backend/src/main/java/com/chat/backend/domain/entities/UserEntity.java
@@ -1,4 +1,24 @@
 package com.chat.backend.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name="users")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+    private Integer id;
+    private String username;
+    private String password;
+    private LocalDateTime last_activity;
 }
