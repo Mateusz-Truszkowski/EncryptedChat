61308391726ad881aaf6da7e832adb10806e8d2c
Mateusz <mateusz.truszkowski002@gmail.com>
2025-06-09 22:01:27 +0200

    Add entities

--- backend/src/main/java/com/chat/backend/domain/entities/GroupEntity.java
+++ backend/src/main/java/com/chat/backend/domain/entities/GroupEntity.java
@@ -1,4 +1,20 @@
 package com.chat.backend.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "sprints")
 public class GroupEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "group_id_seq")
+    private Integer id;
+    private String name;
 }
