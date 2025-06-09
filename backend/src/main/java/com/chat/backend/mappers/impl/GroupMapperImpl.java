65501a5d014e2e25262dd65599f6078915633f63
Mateusz <mateusz.truszkowski002@gmail.com>
2025-06-09 22:29:51 +0200

    Add mappers

--- backend/src/main/java/com/chat/backend/mappers/impl/GroupMapperImpl.java
+++ backend/src/main/java/com/chat/backend/mappers/impl/GroupMapperImpl.java
@@ -1,4 +1,25 @@
 package com.chat.backend.mappers.impl;
 
-public class GroupMapperImpl {
+import com.chat.backend.domain.dto.GroupDto;
+import com.chat.backend.domain.entities.GroupEntity;
+import com.chat.backend.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+
+public class GroupMapperImpl implements Mapper<GroupEntity, GroupDto> {
+
+    private final ModelMapper modelMapper;
+
+    public GroupMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public GroupDto mapTo(GroupEntity entity) {
+        return modelMapper.map(entity, GroupDto.class);
+    }
+
+    @Override
+    public GroupEntity mapFrom(GroupDto GroupDto) {
+        return modelMapper.map(GroupDto, GroupEntity.class);
+    }
 }
--- backend/src/main/java/com/chat/backend/mappers/impl/MessageMapperImpl.java
+++ backend/src/main/java/com/chat/backend/mappers/impl/MessageMapperImpl.java
@@ -1,4 +1,25 @@
 package com.chat.backend.mappers.impl;
 
-public class MessageMapperImpl {
+import com.chat.backend.domain.dto.MessageDto;
+import com.chat.backend.domain.entities.MessageEntity;
+import com.chat.backend.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+
+public class MessageMapperImpl implements Mapper<MessageEntity, MessageDto> {
+
+    private final ModelMapper modelMapper;
+    
+    public MessageMapperImpl(ModelMapper mapper) {
+        this.modelMapper = mapper;
+    }
+
+    @Override
+    public MessageDto mapTo(MessageEntity entity) {
+        return modelMapper.map(entity, MessageDto.class);
+    }
+
+    @Override
+    public MessageEntity mapFrom(MessageDto MessageDto) {
+        return modelMapper.map(MessageDto, MessageEntity.class);
+    }
 }
